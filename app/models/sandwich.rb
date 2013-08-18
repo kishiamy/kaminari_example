2f9c1cdaf8e6f91b30d3c76a4f2ebb1f4a006c65
kishiamy <kishiamy84@gmail.com>
2013-08-18 23:36:56 +0200

    Kaminari example

---
+++ app/models/sandwich.rb
@@ -0,0 +1,5 @@
+class Sandwich
+  include Mongoid::Document
+  field :name, type: String
+  field :description, type: String
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandwiches::Application.config.secret_token = '0ee01956021361bf0238f5ac235dd38279854aa5db2f5dd2caa50801e99959ad6604c9615159d920d80ea83e7ae6f28642907c814bdb1d7df34646f1fb8b99df'
